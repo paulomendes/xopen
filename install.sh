5cc263b59f2a0bea8e2414a8c15b587f12e2d3d9
paulomendes <paulo.mendes@movile.com>
2013-05-09 15:32:41 -0300

    fixing install

--- install.sh
+++ install.sh
@@ -8,15 +8,20 @@ if [  -e /usr/bin/xopen ]; then
 		exit
 	fi
 fi
+echo "\033[0;34mMaking some fucking configurations, maybe I'll need some fucking sudoers permission to accomplish this.\033[0m"
 sudo rm -f /usr/bin/xopen
 echo "\033[0;34mGetting xopen DUDE!\033[0m"
+
+if [ -d ~/.xopen ]; then
+	rm -rf ~/.xopen
+fi
 
 hash git >/dev/null && /usr/bin/env git clone https://github.com/guilhermeandrade/xopen.git ~/.xopen || {
   echo "git not installed"
   exit
 }
 
-echo "\033[0;34mMaking some fucking configurations, maybe I'll need some fucking sudoers permission to accomplish this.\033[0m"
+echo "\033Almost There\033[0m"
 
 sudo mv ~/.xopen/xopen /usr/bin/xopen
 
