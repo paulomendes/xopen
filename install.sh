9baaafaeac5eedf429c52a4a65896c01195ca14a
paulomendes <paulo.mendes@movile.com>
2013-05-09 15:41:00 -0300

    bug fix of install script

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ if [  -e /usr/bin/xopen ]; then
 	echo "You already have xopen installed. You already are a happy guy with a fantastic shor usefull script"
 	echo "Do you want replace it [n/Y]"
 	
-	read -e opt <"tty"
+	read -e opt
 
 	if [[ ${opt} == "n" ]]; then
 		exit
