0ef8a272d245a5b95963f83889b3c6232ee8037c
paulomendes <paulo.mendes@movile.com>
2013-05-09 16:02:56 -0300

    updating installs script

--- install.sh
+++ install.sh
@@ -1,16 +1,6 @@
 if [  -e /usr/bin/xopen ]; then
-	echo "You already have xopen installed. You already are a happy guy with a fantastic shor usefull script"
-	echo "Do you want replace it [n/Y]"
-	
-	while read -t 0 notused; do
-   		read input
-   		echo "ignoring $input"
-	done
-	read -e opt
-
-	if [[ ${opt} == "n" ]]; then
-		exit
-	fi
+	echo "\033[0;33mYou already have xopen installed. You already are a happy guy with a fantastic shor usefull script\033[0m"
+	echo "\033[0;33mWe are justing updating your xopen\033[0m"
 fi
 echo "\033[0;34mMaking some fucking configurations, maybe I'll need some fucking sudoers permission to accomplish this.\033[0m"
 sudo rm -f /usr/bin/xopen
